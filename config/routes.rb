9a054a92c628e95bb0c7d26880b74605e93ced00
pavans <pavan.s@card91.io>
2026-01-24 13:27:43 +0530

    Initial Rails app setup for Ruby 3.1.7 and MySQL

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "static#dashboard"
+  get 'people/:id', to: 'static#person', as: 'person'
+
+  resources :expenses, only: [:new, :create, :index, :show, :destroy]
+  resources :payments, only: [:new, :create]
+end
