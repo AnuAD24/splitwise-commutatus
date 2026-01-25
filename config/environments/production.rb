e3e40e3de6dfe17c0e9f2da0794d20ef9a5ed107
pavans <pavan.s@card91.io>
2026-01-25 21:59:40 +0530

    Allow onrender.com hosts in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,9 +48,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Allow Render host in production.
-  config.hosts << "splitwise-commutatus-663d.onrender.com"
-  config.hosts << "splitwise-commutatus-1-gyic.onrender.com"
+  # Allow Render hosts in production.
+  config.hosts << /.*\.onrender\.com/
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
